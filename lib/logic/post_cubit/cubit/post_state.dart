ac25b6531fb34432642787e5d100666593c256bb
Amir55143 <aamirnoor55143@gmail.com>
2023-10-17 11:54:58 +0500

    Post_State

---
+++ lib/logic/post_cubit/cubit/post_state.dart
@@ -0,0 +1,16 @@
+part of 'post_cubit.dart';
+
+@immutable
+abstract class PostState {}
+
+class PostLoadingState extends PostState {}
+
+class PostLoadedState extends PostState {
+  final List<PostModel> posts;
+  PostLoadedState(this.posts);
+}
+
+class PostErrorState extends PostState {
+  final String error;
+  PostErrorState(this.error);
+}
